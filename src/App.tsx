02ab487aefb2383c638279c9f56cc04f31b50591
Nik91K <nikotakovsl@gmail.com>
2025-10-04 21:34:51 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import LayoutPage from './layout'
+
+function App() {
+
+  return (
+    <LayoutPage>
+      <div>
+        
+      </div>
+    </LayoutPage>
+  )
+}
+
+export default App
